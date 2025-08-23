09acc427398377352c235becdcf10acac6f4d2b9
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-08-23 22:50:18 +0200

    #15: Cleaned up views.py a bit (there is probably more to come as we continue our work)

--- graph_explorer/graph_explorer/views.py
+++ graph_explorer/graph_explorer/views.py
@@ -14,41 +14,25 @@ def index(request):
         fs = FileSystemStorage()
         filename = fs.save(uploaded_file.name, uploaded_file)
 
-        # Get full system path to the saved file
         file_path = fs.path(filename)
 
+    plugin_service = apps.get_app_config('graph_explorer').plugin_service
     visualizer_identifier = request.GET.get("visualizer")
     data_source_identifier = request.GET.get("datasource")
 
-    plugin_service = apps.get_app_config('graph_explorer').plugin_service
-
     data_source_plugins = plugin_service.plugins[DATA_SOURCE_GROUP]
-    selected_data_source_plugin = data_source_plugins[0] if data_source_plugins else None
-    if data_source_identifier:
-        for plugin in data_source_plugins:
-            if plugin.identifier() == data_source_identifier:
-                selected_data_source_plugin = plugin
-                break
-    
-    graph = selected_data_source_plugin.load(path=file_path) if file_path else Graph()
+    selected_data_source_plugin = plugin_service.get_selected_plugin(group=DATA_SOURCE_GROUP, identifier=data_source_identifier)
 
     visualizer_plugins = plugin_service.plugins[VISUALIZER_GROUP]
+    selected_visualizer_plugin = plugin_service.get_selected_plugin(group=VISUALIZER_GROUP, identifier=visualizer_identifier)
 
-    print("PLUGINS: " + str(visualizer_plugins))
-
-    selected = visualizer_plugins[0] if visualizer_plugins else None
-    if visualizer_identifier:
-        for plugin in visualizer_plugins:
-            if plugin.identifier() == visualizer_identifier:
-                selected = plugin
-                break
-
-    graph_html = selected.visualize(graph) if selected else "No visualizer selected 🚫"
+    graph = selected_data_source_plugin.load(path=file_path) if file_path else Graph()
+    graph_html = selected_visualizer_plugin.visualize(graph) if selected_visualizer_plugin else "No visualizer selected 🚫"
 
     return render(request, "index.html", {
         "visualizer_plugins": visualizer_plugins,
         "data_source_plugins": data_source_plugins,
         "graph_html": graph_html,
-        "selected_visualizer_plugin": selected.identifier() if selected else None,
+        "selected_visualizer_plugin": selected_visualizer_plugin.identifier() if selected_visualizer_plugin else None,
         "selected_data_source_plugin": selected_data_source_plugin.identifier() if selected_data_source_plugin else None
     })
--- platform/src/use_cases/plugin_recognition.py
+++ platform/src/use_cases/plugin_recognition.py
@@ -1,12 +1,12 @@
 from typing import List
 import pkg_resources
 
-from graph_explorer_api.plugins.visualizer_plugin import VisualizerPlugin 
+from graph_explorer_api.plugins.base import Plugin
 
 class PluginService(object):
 
     def __init__(self):
-        self.plugins: dict[str,List[VisualizerPlugin]] = {}
+        self.plugins: dict[str,List[Plugin]] = {}
 
     def load_plugins(self, group: str):
         """
@@ -19,3 +19,11 @@ class PluginService(object):
             p = entry.load()
             plugin = p()
             self.plugins[group].append(plugin)
+
+    def get_selected_plugin(self, group: str, identifier: str) -> Plugin:
+        if identifier:
+            for plugin in self.plugins[group]:
+                if plugin.identifier() == identifier:
+                    return plugin
+
+        return self.plugins[group][0] if self.plugins[group] else None
