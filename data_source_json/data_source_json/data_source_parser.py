a85aa57ca57bcc01867601ced4fea9d2c02325cf
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-08-24 17:37:59 +0200

    #16: add configuration, data source parser and loader for data source JSON plugin

---
+++ data_source_json/data_source_json/data_source_parser.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from api.graph_explorer_api.model.graph import Graph
+from api.graph_explorer_api.plugins.data_source_plugin import DataSourcePlugin
+from data_source_json.data_source_json.configuration import JSONLoaderType, JSONGraphType
+from data_source_json.data_source_json.configuration import Configuration
+from data_source_json.data_source_json.loader import JSONFileLoader, JSONLoader, JSONUrlLoader
+from data_source_json.data_source_json.parser import JSONParser
+
+class DataSourceJSONParser(DataSourcePlugin):
+    loader: JSONLoader = JSONFileLoader()
+    config: Configuration | None = Configuration()
+    parser: JSONParser = JSONParser()
+
+    def load(self, **kwargs) -> Graph:
+        self.__configure_plugin()
+        data = self.loader.load(kwargs.get("path"))
+        graph_type = self.config.get_graph_type() if self.config else "directed"
+        return self.parser.parse(data, graph_type)
+
+    def __configure_plugin(self):
+        loader_type = self.config.get_loader_type()
+        if loader_type == JSONLoaderType.URL:
+            self.loader = JSONUrlLoader()
+        else:
+            self.loader = JSONFileLoader()
+
+    def identifier(self) -> str:
+        return "DataSourceJSONParser"
+
+    def name(self) -> str:
+        return "Data source JSON parser"
