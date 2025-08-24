29ed438db2e6fbfcefcbd6cac19c844f7da79d87
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-08-24 22:43:58 +0200

    #23: add safe conversion method for datetime in Block Visualizer to fix UI error

--- block_visualizer/block_visualizer/block_visualizer.py
+++ block_visualizer/block_visualizer/block_visualizer.py
@@ -1,4 +1,6 @@
 import os
+from datetime import date, datetime
+
 from jinja2 import FileSystemLoader, Environment
 from graph_explorer_api.plugins.visualizer_plugin import VisualizerPlugin 
 from graph_explorer_api.model.graph import Graph
@@ -11,6 +13,13 @@ class BlockVisualizerPlugin(VisualizerPlugin):
   def identifier(self) -> str:
     return "block_visualizer"
 
+  def safe_value(self, value):
+      if isinstance(value, datetime):
+          return value.strftime("%d.%m.%Y. %H:%M")  # "24.08.2025. 21:15" (date and time)
+      elif isinstance(value, date):
+          return value.strftime("%d.%m.%Y.")  # "24.08.2025." (only date)
+      return str(value)
+
   def visualize(self, graph: Graph, **kwargs) -> str:
     p = os.path.dirname(__file__)
     path = os.path.join(p, "templates")
@@ -22,7 +31,7 @@ class BlockVisualizerPlugin(VisualizerPlugin):
             "id": str(n.id),
             "name": str(n.data.get('name', n.id)) if n.data.get('name', n.id) is not None else str(n.id),
             "attributes": [
-                {"name": key, "value": value}
+                {"name": key, "value": self.safe_value(value)}
                 for key, value in n.data.items()
                 if key is not None and value is not None and key != 'name'
             ]
