c055795f7e5b7edf74551b104eccf5850c6ecf87
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-08-24 17:38:29 +0200

    #16: add parser for data source JSON plugin (currently works with file path)

---
+++ data_source_json/data_source_json/parser.py
@@ -0,0 +1,106 @@
+import json
+
+from datetime import datetime
+
+from api.graph_explorer_api.model.edge import Edge
+from api.graph_explorer_api.model.graph import Graph
+from api.graph_explorer_api.model.node import Node
+from data_source_json.data_source_json.configuration import JSONGraphType
+
+class JSONParser:
+    __graph: Graph = Graph()
+    __nodes: dict[str, Node] = {}
+    __edges_to_resolve: list[tuple[str, str]] = []  # (from_id, to_id)
+
+    def parse(self, data: str, graph_type: JSONGraphType) -> Graph:
+        self.__reset()  # so different instances won't use the same values
+        self.__graph.directed = graph_type == JSONGraphType.DIRECTED.value
+
+        json_data = json.loads(data)
+        self.__build_graph(json_data, parent_id=None)
+        self.__resolve_edges()
+
+        return self.__graph
+
+    def __reset(self):
+        self.__graph = Graph()
+        self.__nodes = {}
+        self.__edges_to_resolve = []
+
+    def __build_graph(self, json_data, parent_id=None):
+        if not isinstance(json_data, dict):
+            return
+
+        root_node_id = json_data.get("@id")
+        if not root_node_id:
+            root_node_id = f"{len(self.__nodes)}"  # if no @id => generate value
+
+        node_data = {}
+        for key, val in json_data.items():
+            if isinstance(val, (dict, list)):
+                continue
+            if key not in ["@id", "parent"]:
+                node_data[key] = self.__convert_value(val)
+
+        if root_node_id not in self.__nodes:
+            node = Node(root_node_id, node_data)
+            self.__nodes[root_node_id] = node
+            self.__graph.nodes.append(node)
+        else:
+            self.__nodes[root_node_id].data.update(node_data)
+
+        current_node = self.__nodes[root_node_id]
+
+        # add `parent -> current` edge if it exists
+        if parent_id and parent_id in self.__nodes:
+            self.__add_edge(self.__nodes[parent_id], current_node)
+
+        if "parent" in json_data:
+            self.__edges_to_resolve.append((root_node_id, json_data["parent"]))
+
+        # go through children
+        for _, val in json_data.items():
+            if isinstance(val, dict):
+                self.__build_graph(val, parent_id=root_node_id)
+            elif isinstance(val, list):
+                for item in val:
+                    self.__build_graph(item, parent_id=root_node_id)
+
+    """
+    Add an edge depending on graph type (directed/undirected).
+    """
+    def __add_edge(self, from_node: Node, to_node: Node):
+        if self.__graph.directed:
+            if not self.__edge_exists(from_node, to_node):
+                self.__graph.edges.append(Edge(from_node, to_node))
+        else:
+            if not self.__edge_exists(from_node, to_node):
+                self.__graph.edges.append(Edge(from_node, to_node))
+            if not self.__edge_exists(to_node, from_node):
+                self.__graph.edges.append(Edge(to_node, from_node))
+
+    def __edge_exists(self, a: Node, b: Node) -> bool:
+        return any(e.from_node == a and e.to_node == b for e in self.__graph.edges)
+
+    """
+    Resolve parent/child edges after building the graph.
+    """
+    def __resolve_edges(self):
+        for src_id, target_id in self.__edges_to_resolve:
+            if src_id in self.__nodes and target_id in self.__nodes:
+                self.__add_edge(self.__nodes[src_id], self.__nodes[target_id])
+
+    def __convert_value(self, value):
+        if isinstance(value, str):
+            if value.isdigit():
+                return int(value)
+            try:
+                return float(value)
+            except ValueError:
+                pass
+            try:
+                return datetime.fromisoformat(value)
+            except ValueError:
+                pass
+            return value  # string
+        return value
