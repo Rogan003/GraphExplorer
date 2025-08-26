925b663f97a6ceabb9fc40986215f900d83f3eaf
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-08-26 02:06:28 +0200

    #21: add urls for applying and resetting filters graph

--- graph_explorer/graph_explorer/urls.py
+++ graph_explorer/graph_explorer/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from graph_explorer import views
 
 urlpatterns = [
-    path('', views.index, name='index'), 
+    path('', views.index, name='index'),
+    path("workspace/<int:workspace_id>/apply_filter/", views.apply_filter, name="apply_filter"),
+    path("workspace/<int:workspace_id>/reset_filters/", views.reset_filters, name="reset_filters"),
 ]
