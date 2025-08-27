4affb0dcd03598f2cf9ed337f7f501b8449f39d8
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-08-27 02:35:17 +0200

    #21: add Filter class

--- platform/src/use_cases/workspace/workspace.py
+++ platform/src/use_cases/workspace/workspace.py
@@ -1,12 +1,14 @@
 from graph_explorer_api.model.graph import Graph
 from use_cases.const import DATA_SOURCE_GROUP, VISUALIZER_GROUP
+from use_cases.filter.filter import Filter
 
 class Workspace:
-    def __init__(self, id, file_path=None, data_source_identifier=None, visualizer_identifier=None):
+    def __init__(self, id, file_path=None, data_source_identifier=None, visualizer_identifier=None, filters=None):
         self.id = id
         self.file_path = file_path
         self.data_source_identifier = data_source_identifier
         self.visualizer_identifier = visualizer_identifier
+        self.filters = filters or []
 
     def load_graph(self, plugin_service, tree_view_service):
         data_source = plugin_service.get_selected_plugin(DATA_SOURCE_GROUP, self.data_source_identifier)
@@ -17,18 +19,45 @@ class Workspace:
         else:
             self.graph = Graph()
 
+        filtered_graph = self.get_filtered_graph()
+
         if visualizer:
-            self.graph_html = visualizer.visualize(self.graph)
+            self.graph_html = visualizer.visualize(filtered_graph)
         else:
             self.graph_html = "No visualizer selected 🚫"
 
-        self.tree_view = tree_view_service.generate_template(self.graph)
+        self.tree_view = tree_view_service.generate_template(filtered_graph)
 
-    
     def to_dict(self):
         return {
             "id": self.id,
             "file_path": self.file_path,
             "data_source_identifier": self.data_source_identifier,
             "visualizer_identifier": self.visualizer_identifier,
+            "filters": [f.to_dict() for f in self.filters],
         }
+
+    @classmethod
+    def from_dict(cls, data):
+        filters = [Filter.from_dict(fd) for fd in data.get("filters", [])]
+        return cls(
+            id=data["id"],
+            file_path=data.get("file_path"),
+            data_source_identifier=data.get("data_source_identifier"),
+            visualizer_identifier=data.get("visualizer_identifier"),
+            filters=filters,
+        )
+
+    def add_filter(self, filter_obj: Filter):
+        self.filters.append(filter_obj)
+
+    def clear_filters(self):
+        self.filters = []
+
+    def get_filtered_graph(self) -> Graph:
+        g = self.graph
+
+        for f in self.filters:
+            g = g.apply_filters(f.to_dict())
+
+        return g
