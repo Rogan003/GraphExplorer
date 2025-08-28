685a24e2f83072240765123c7a65fe7d41aae09a
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-08-28 04:53:15 +0200

    #21: integrate filters with workspaces in views.py

--- graph_explorer/graph_explorer/views.py
+++ graph_explorer/graph_explorer/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponseBadRequest
 from django.shortcuts import render, redirect
 from django.apps import apps
 from use_cases.const import DATA_SOURCE_GROUP, VISUALIZER_GROUP
@@ -9,6 +8,9 @@ from use_cases.workspace.workspace_service import (
     create_workspace,
     update_workspace_in_session,
 )
+
+from use_cases.filter.filter import Filter
+
 
 def index(request):
     # request.session.flush()
@@ -42,14 +44,13 @@ def index(request):
             tree_view_service=tree_view_service,
         )
 
-    """
-    f = Filter(attribute_name="name", comparator="contains", attribute_value=None, search_value="Alice")
-    ws.add_filter(f)
-    
-    request.session["workspace"] = ws.to_dict()
-    saved = request.session["workspace"]
-    ws = Workspace.from_dict(saved)
-    """
+    if request.GET.get("reset_filters"):
+        return reset_filters(request, active_workspace)
+    else:
+        filter_error_msg = apply_filters(request, active_workspace)
+
+    if active_workspace and filter_error_msg is None:
+        active_workspace.load_graph(plugin_service, tree_view_service)
 
     return render(request, "index.html", {
         "visualizer_plugins": plugin_service.plugins[VISUALIZER_GROUP],
@@ -60,74 +61,41 @@ def index(request):
         "tree_view": active_workspace.tree_view if active_workspace else None,
         "workspaces": workspaces,
         "active_workspace_id": active_ws_id,
+        "filter_error_msg": filter_error_msg,
     })
 
-def apply_filter(request):
-    filter_error_msg= None
+def apply_filters(request, ws):
+    filter_error_msg = None
 
-    if request.method == "POST":
-        ws_id = request.POST.get("active_workspace_id") or request.GET.get("tab")
+    if ws is not None:
+        filters = Filter (
+            request.POST.get("filter_attribute_name"),
+            request.POST.get("filter_comparator"),
+            request.POST.get("filter_attribute_value"),
+            request.POST.get("filter_search"),
+        )
 
-        if ws_id is None:
-            filter_error_msg = "No workspace ID provided."
-        else:
-            ws_id = int(ws_id)
-            ws, _ = get_active_workspace(request.session, ws_id)
-            if not ws:
-                filter_error_msg = "Workspace not found."
-            else:
-                filters = {
-                    "attribute_name": request.POST.get("filter_attribute_name"),
-                    "comparator": request.POST.get("filter_comparator"),
-                    "attribute_value": request.POST.get("filter_attribute_value"),
-                    "search": request.POST.get("filter_search"),
-                }
+        try:
+            ws.add_filter(filters)
+            update_workspace_in_session(request.session, ws)
 
-                try:
-                    ws.add_filter(filters)
-                    plugin_service = apps.get_app_config("graph_explorer").plugin_service
-                    tree_view_service = apps.get_app_config("graph_explorer").tree_view_service
-                    ws.load_graph(plugin_service, tree_view_service)
-                    update_workspace_in_session(request.session, ws)
-                except ValueError:
-                    filter_error_msg = "Invalid filter."
+        except ValueError:
+            filter_error_msg = "Invalid filter."
 
-        plugin_service = apps.get_app_config("graph_explorer").plugin_service
-        tree_view_service = apps.get_app_config("graph_explorer").tree_view_service
-        workspaces = handle_initial_workspace(request.session, plugin_service, tree_view_service)
-        active_workspace, _ = get_active_workspace(request.session, ws_id or 0)
+    return filter_error_msg
 
-        return render(request, "index.html", {
-            "visualizer_plugins": plugin_service.plugins[VISUALIZER_GROUP],
-            "data_source_plugins": plugin_service.plugins[DATA_SOURCE_GROUP],
-            "graph_html": active_workspace.graph_html if active_workspace else "No graph yet",
-            "selected_visualizer_identifier": active_workspace.visualizer_identifier if active_workspace else None,
-            "selected_data_source_identifier": active_workspace.data_source_identifier if active_workspace else None,
-            "tree_view": active_workspace.tree_view if active_workspace else None,
-            "workspaces": workspaces,
-            "active_workspace_id": ws_id or 0,
-            "filter_error_msg": filter_error_msg,
-        })
+def reset_filters(request, ws):
+    if ws is not None:
+        ws.clear_filters()
+        update_workspace_in_session(request.session, ws)
 
-    return HttpResponseBadRequest("Error")
+    query_params = request.GET.copy()
+    while "reset_filters" in query_params:
+        query_params.pop("reset_filters", None)
 
+    url = request.path
 
-def reset_filters(request):
-    ws_id = request.POST.get("workspace_id") or request.GET.get("tab")
-    if ws_id is None:
-        return HttpResponseBadRequest("No workspace ID provided.")
-    ws_id = int(ws_id)
+    if query_params:
+        url += "?" + query_params.urlencode()
 
-    ws, _ = get_active_workspace(request.session, ws_id)
-    if not ws:
-        return HttpResponseBadRequest("Workspace not found.")
-
-    ws.clear_filters()
-
-    plugin_service = apps.get_app_config("graph_explorer").plugin_service
-    tree_view_service = apps.get_app_config("graph_explorer").tree_view_service
-    ws.load_graph(plugin_service, tree_view_service)
-
-    update_workspace_in_session(request.session, ws)
-
-    return redirect("index", tab=ws_id)
+    return redirect(url)
