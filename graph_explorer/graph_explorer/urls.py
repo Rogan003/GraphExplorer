8bc5b001579aea7f43486e3c29de1b1ad805b8d7
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-08-28 04:50:20 +0200

    #21: delete paths for apply/reset filters in urls.py

--- graph_explorer/graph_explorer/urls.py
+++ graph_explorer/graph_explorer/urls.py
@@ -3,6 +3,4 @@ from graph_explorer import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("workspace/<int:workspace_id>/apply_filter/", views.apply_filter, name="apply_filter"),
-    path("workspace/<int:workspace_id>/reset_filters/", views.reset_filters, name="reset_filters"),
 ]
