a8f6d8b6b2161e47d57871d085b37d89c5b44581
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-09-05 01:43:01 +0200

    #44: Added support for edge ID and data in API and data source XML plugin

--- api/graph_explorer_api/model/edge.py
+++ api/graph_explorer_api/model/edge.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from .node import Node
 
 @dataclass
@@ -7,22 +7,31 @@ class Edge:
     Represents an edge (connection) between two nodes in a graph.
 
     Attributes:
+        id (int): Unique identifier for the edge.
         from_node (Node): The starting node of the edge.
         to_node (Node): The ending node of the edge.
+        data (dict): Optional dictionary holding additional attributes about the edge.
+                     Defaults to an empty dictionary if not provided. Can store
+                     arbitrary attributes like labels, names, or other metadata.
     """
-    # TODO: add id
+    id: int
     from_node: Node
     to_node: Node
+    data: dict = field(default_factory=dict)
 
     def to_dict(self):
         return {
+            "id": self.id,
             "from_node": self.from_node.to_dict(),
             "to_node": self.to_node.to_dict(),
+            "data": self.data
         }
 
     @classmethod
     def from_dict(cls, data):
         return cls(
+            id=data["id"],
             from_node=Node.from_dict(data["from_node"]),
-            to_node=Node.from_dict(data["to_node"])
+            to_node=Node.from_dict(data["to_node"]),
+            data=data.get("data", {})
         )
--- data_source_xml/data_source_xml/main.py
+++ data_source_xml/data_source_xml/main.py
@@ -1,6 +1,12 @@
 from data_source_xml.data_source_parser import DataSourceXmlParser
+from graph_explorer_api.plugins.data_source_loaders import DataSourceFileLoader
 
 if __name__ == "__main__":
     parser = DataSourceXmlParser()
+    parser.configure_plugin(
+            reference_attribute = "reference",
+            loader = DataSourceFileLoader(),
+            is_graph_directed = True
+        )
     graph = parser.load(path="test_files/test.xml")
     print(graph)
