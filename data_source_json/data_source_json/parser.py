86cdcf8da8744ec64c42d1986c256993995da452
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-09-05 18:23:32 +0200

    #44: update JSON parser to support IDs and data in Edges

--- data_source_json/data_source_json/parser.py
+++ data_source_json/data_source_json/parser.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import json
 
 from datetime import datetime
@@ -9,8 +11,10 @@ from graph_explorer_api.model.node import Node
 class JSONParser:
     __graph: Graph = Graph()
     __nodes: dict[str, Node] = {}  # node_id -> Node
-    __edges_to_resolve: list[tuple[str, str]] = []  # (from_id, to_id)
-    __reference_attribute: str = "reference"
+    __edges_cnt: int = 0
+    __nodes_cnt: int = 0
+    __edges_to_resolve: list[tuple[str, str, dict]] = []  # (from_id, to_id, edge_data)
+    __reference_attribute: str = "parent"
 
     def parse(self, data: str, reference_attribute: str, is_graph_directed: bool) -> Graph:
         self.__reset()  # so different instances won't use the same values
@@ -26,6 +30,8 @@ class JSONParser:
     def __reset(self):
         self.__graph = Graph()
         self.__nodes = {}
+        self.__edges_cnt = 0
+        self.__nodes_cnt = 0
         self.__edges_to_resolve = []
 
     def __build_graph(self, json_data, parent_id=None):
@@ -33,14 +39,15 @@ class JSONParser:
             return
 
         # if no @id => generate value
-        root_node_id = json_data.get("@id") or str(len(self.__nodes))
+        root_node_id = str(self.__nodes_cnt)
+        self.__nodes_cnt += 1
 
         node_data = {}
         for key, val in json_data.items():
             if isinstance(val, (dict, list)):
                 continue
             # collect node attributes
-            if key not in ["@id", self.__reference_attribute]:
+            if key not in ["@id", self.__reference_attribute, "edge_data"]:
                 node_data[key] = self.__convert_value(val)
 
         if root_node_id not in self.__nodes:
@@ -54,13 +61,18 @@ class JSONParser:
 
         # add `parent -> current` edge if it exists
         if parent_id and parent_id in self.__nodes:
-            self.__add_edge(self.__nodes[parent_id], current_node)
+            edge_data = {k: self.__convert_value(v) for k, v in json_data.get("edge_data", {}).items()}
+            self.__add_edge(self.__nodes[parent_id], current_node, edge_data)
 
         if self.__reference_attribute in json_data:
-            self.__edges_to_resolve.append((str(root_node_id), str(json_data[self.__reference_attribute])))
+            edge_data = {k: self.__convert_value(v) for k, v in json_data.get("edge_data", {}).items()}
+            self.__edges_to_resolve.append((str(root_node_id), str(json_data[self.__reference_attribute]), edge_data))
 
         # go through children
-        for _, val in json_data.items():
+        for key, val in json_data.items():
+            if key == "edge_data":
+                continue
+
             if isinstance(val, dict):
                 self.__build_graph(val, parent_id=root_node_id)
             elif isinstance(val, list):
@@ -70,15 +82,20 @@ class JSONParser:
     """
     Add an edge depending on graph type (directed/undirected).
     """
-    def __add_edge(self, from_node: Node, to_node: Node):
+    def __add_edge(self, from_node: Node, to_node: Node, edge_data: dict | None = None):
+        edge_attrs = {k: self.__convert_value(v) for k, v in (edge_data or {}).items()}
+
         if self.__graph.directed:
             if not self.__edge_exists(from_node, to_node):
-                self.__graph.edges.append(Edge(from_node, to_node))
+                self.__graph.edges.append(Edge(id=self.__edges_cnt, from_node=from_node, to_node=to_node, data=edge_attrs))
+                self.__edges_cnt += 1
         else:
             if not self.__edge_exists(from_node, to_node):
-                self.__graph.edges.append(Edge(from_node, to_node))
+                self.__graph.edges.append(Edge(id=self.__edges_cnt, from_node=from_node, to_node=to_node, data=edge_attrs))
+                self.__edges_cnt += 1
             if not self.__edge_exists(to_node, from_node):
-                self.__graph.edges.append(Edge(to_node, from_node))
+                self.__graph.edges.append(Edge(id=self.__edges_cnt, from_node=from_node, to_node=to_node, data=edge_attrs))
+                self.__edges_cnt += 1
 
     def __edge_exists(self, a: Node, b: Node) -> bool:
         return any(e.from_node == a and e.to_node == b for e in self.__graph.edges)
@@ -87,11 +104,12 @@ class JSONParser:
     Resolve parent/child edges after building the graph.
     """
     def __resolve_edges(self):
-        for src_id, target_id in self.__edges_to_resolve:
+        for src_id, target_id, edge_data in self.__edges_to_resolve:
             if src_id in self.__nodes and target_id in self.__nodes:
-                self.__add_edge(self.__nodes[src_id], self.__nodes[target_id])
+                self.__add_edge(self.__nodes[src_id], self.__nodes[target_id], edge_data)
 
-    def __convert_value(self, value):
+    @staticmethod
+    def __convert_value(value):
         if isinstance(value, str):
             if value.isdigit():
                 return int(value)
