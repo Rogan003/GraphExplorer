f1021983abd665a535016c17693ffeb25cfdc883
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-09-05 18:22:16 +0200

    #44: update main.py for Data Source JSON parser (for testing)

--- data_source_json/data_source_json/main.py
+++ data_source_json/data_source_json/main.py
@@ -1,6 +1,12 @@
 from data_source_json.data_source_parser import DataSourceJSONParser
+from graph_explorer_api.plugins.data_source_loaders import DataSourceFileLoader
 
 if __name__ == "__main__":
     parser = DataSourceJSONParser()
+    parser.configure_plugin(
+        reference_attribute="parent",
+        loader=DataSourceFileLoader(),
+        is_graph_directed=True
+    )
     graph = parser.load(path="test_files/test.json")
     print(graph)
